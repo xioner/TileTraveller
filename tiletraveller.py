e0bf8ddf6585f9970c37306f0cb40bb94817f53d
xioner <skikunastom@yahoo.com>
2018-09-18 11:35:08 +0000

    garbage version

--- tiletraveller.py
+++ tiletraveller.py
@@ -7,16 +7,125 @@
 # If the player enters an invalid direction, the program prints “Not a valid direction!” 
 # and allows the player to enter the direction again.
 
-# First check where the player is
+# (1,1) in ('north')
+# (1,2) in ('north', 'south', 'east')
+# (1,3) in ('north', 'east')
+# (2,1) in ('east')
+# (2,2) in ('west', 'south')
+# (2,3) in ('north')
+# (3,3) in ('west', 'south')
+# (3,2) in ('north', 'south')
 
-# 1,1 = UP
-# 1,2 = North/South/east
-# 1,3 = South/east
-# 2,1 = East
-# 2,2 = West/South
-# 2,1 = North
-# 3,3 = West/South
-# 3,2 = North/South
-# 3,1 = Victory
+N = '(N)orth'
+S = '(S)outh'
+E = '(E)east'
+W = '(W)est'
 
-print("boo")
+
+
+x_pos, y_pos = 1,1
+player_start = x_pos, y_pos
+# start = print (f"You can travel: (N)orth")
+
+
+
+while True: 
+    if x_pos == 1 and y_pos == 1:
+        print("You can travel (N)orth")
+    direction = str(input("Direction: "))
+    if direction.lower() == 'n':
+        y_pos += 1
+    else: 
+        print ("Not a valid direction!")
+
+    if x_pos == 1 and y_pos == 2:
+        print(f"You can travel: {N}, {S}, {E}")
+        direction = str(input("Direction: "))
+        if direction.lower() == 'n' or 's' or 'e':
+            if direction.lower() == 'n':
+                y_pos += 1
+                print ('Moved north')
+            if direction.lower() == 's':
+                y_pos -= 1
+                print('Moved south')
+            if direction.lower() == 'e': 
+                x_pos += 1
+                print ('Moved east')
+        else: 
+            print("Not a valid direction!")
+
+    if x_pos == 1 and y_pos == 3:
+        print(f"You can travel: {S}, {E}")
+        direction = str(input("Direction: "))
+        if direction.lower() == 's' or 'e':
+            if direction.lower() == 's':
+                y_pos -= 1
+                print('Moved south')
+            if direction.lower() == 'e': 
+                x_pos += 1
+                print ('Moved east')
+        else: 
+            print("Not a valid direction!")
+
+    if x_pos == 2 and y_pos == 1:
+        print(f"You can travel: {E}")
+        direction = str(input("Direction: "))
+        if direction.lower() == 'e': 
+                x_pos += 1
+                print ('Moved east')
+        else: 
+            print("Not a valid direction!")
+
+    if x_pos == 2 and y_pos == 2:
+        print(f"You can travel: {W}, {S}")
+        direction = str(input("Direction: "))
+        if direction.lower() == 's' or 'w':
+            if direction.lower() == 's':
+                y_pos -= 1
+                print('Moved south')
+            if direction.lower() == 'w': 
+                x_pos -= 1
+                print ('Moved west')
+        else: 
+            print("Not a valid direction!")
+
+    if x_pos == 2 and y_pos == 3:
+        print(f"You can travel: {E}")
+        direction = str(input("Direction: "))
+        if direction.lower() == 'e':
+            if direction.lower() == 'e':
+                x_pos += 1
+                print('Moved east')
+        else: 
+            print("Not a valid direction!")           
+
+    if x_pos == 3 and y_pos == 3:
+        print(f"You can travel: {W}, {S}")
+        direction = str(input("Direction: "))
+        if direction.lower() == 's' or 'w':
+            if direction.lower() == 's':
+                y_pos -= 1
+                print('Moved south')
+            if direction.lower() == 'w': 
+                x_pos -= 1
+                print ('Moved west')
+        else: 
+            print("Not a valid direction!")
+
+    if x_pos == 3 and y_pos == 2:
+        print(f"You can travel: {N}, {S}")
+        direction = str(input("Direction: "))
+        if direction.lower() == 'n' or 's':
+            if direction.lower() == 'n':
+                y_pos += 1
+                print('Moved north')
+            if direction.lower() == 's': 
+                y_pos -= 1
+                print ('Moved south')
+        else: 
+            print("Not a valid direction!")
+
+    if x_pos == 3 and y_pos == 1:
+        print (5*"\nVictory!!")
+        exit()
+    
