4d2902ab5da8407b5f581e0cb25b6f2282c13774
xioner <skikunastom@yahoo.com>
2018-09-18 09:36:15 +0000

    first commit

---
+++ tiletraveller.py
@@ -0,0 +1,22 @@
+# The player enters:
+# • n/N for north (up)
+# • e/E for east (right)
+# • s/S for south (down)
+# • w/W for west (left)
+
+# If the player enters an invalid direction, the program prints “Not a valid direction!” 
+# and allows the player to enter the direction again.
+
+# First check where the player is
+
+# 1,1 = UP
+# 1,2 = North/South/east
+# 1,3 = South/east
+# 2,1 = East
+# 2,2 = West/South
+# 2,1 = North
+# 3,3 = West/South
+# 3,2 = North/South
+# 3,1 = Victory
+
+print("boo")
